47032e75dfcc86f068bd5f35946a668bb2b98be1
mansibansal9648 <mansibansal12345@gmail.com>
2024-07-31 17:03:22 +0530

    initial setup

---
+++ src/main/java/com/eComGenius/demo/EComGeniusApplication.java
@@ -0,0 +1,13 @@
+package com.eComGenius.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComGeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComGeniusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eComGenius/demo/EComGeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eComGenius.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComGeniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
